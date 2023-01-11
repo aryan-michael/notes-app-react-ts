656855301e042ad1727f69c316941c2ae8f3898a
aryan-michael <aryanmichael24@gmail.com>
2023-01-12 01:17:41 +0530

    testing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return ( 
+    <Routes>
+      <Route path="/" element={<h1>Aryan</h1>} />  
+    </Routes>
+    )
+}
+
+export default App
