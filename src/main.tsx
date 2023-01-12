e786bcd03973b5fad055d03c876dc045e2675788
aryan-michael <aryanmichael24@gmail.com>
2023-01-12 18:38:27 +0530

    basic routing | continue with video

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BroswerRouter>
+    <BrowserRouter>
       <App />
-    </BroswerRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
