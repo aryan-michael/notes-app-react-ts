efb8ea7dc2e6f3ffdea045a1c9f4df5120c5f024
aryan-michael <aryanmichael24@gmail.com>
2023-01-16 19:37:10 +0530

    30:46

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,67 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useMemo } from "react"
 import {Container} from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
+import {NewNote} from "./NewNote"
+import { useLocalStorage } from "./useLocalStorage"
+import { v4 as uuidV4 } from "uuid"
+
+export type Note = { //NoteData with ID attached to it
+  id: string
+} & NoteData  //adding NoteData to Note
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  function onCreateNote(data: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes, 
+        {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)},
+      ]
+    })
+  }
 
   return ( 
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New-Home</h1>} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote}/>} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>}/>
           <Route path="edit" element={<h1>Edit</h1>}/>
         </Route>
-        <Route path="*" element={<Navigate to="/" />} />   
+        <Route path="*" element={<Navigate to="/" />} /> /* for when we go to a url that doesn't exist|
+        Navigate redirects to the stipulated url|
+        * matches everything in the url that doesn't exist */
       </Routes>
     </Container>
     )
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,3 +1,15 @@
-export function NewNote(){
-	return <h1>Wassup@!</h1>
+import { NoteForm } from "./NoteForm"
+import { NoteData } from "./App"
+
+type NewNoteProps = {
+	onSubmit: (data: NoteData) => void
+}
+
+export function NewNote({onSubmit}: NewNoteProps){
+	return (
+		<>
+			<h1 className="mb-4">NewNote</h1>
+			<NoteForm onSubmit={onSubmit}/>
+		</>
+		)
 }
